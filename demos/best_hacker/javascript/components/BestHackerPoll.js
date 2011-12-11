18e065b73d5c823fa7137796afbc6ae4ab72620b
Daniel Kim <danieldkimster@gmail.com>
2011-12-11 18:10:21 -0500

    Best Hacker app

---
+++ demos/best_hacker/javascript/components/BestHackerPoll.js
@@ -0,0 +1,15 @@
+dojo.provide('client.components.BestHackerPoll');
+
+mulberry.component('BestHackerPoll', {
+  componentTemplate : dojo.cache('client.components', 'BestHackerPoll/BestHackerPoll.haml'),
+
+  prep : function() {
+    this.hackers = this.baseObj.getData('Hacker').hackers;
+    dojo.forEach(this, hackers, function(hacker) {
+
+    });
+  },
+
+  init : function() {
+  }
+});
---
+++ demos/best_hacker/pages/home.md
@@ -0,0 +1,10 @@
+---
+title: Home
+template:
+  phone: best-hacker
+  tablet: best-hacker
+data:
+  - hackers.yml
+---
+
+Vote for the best hacker.
